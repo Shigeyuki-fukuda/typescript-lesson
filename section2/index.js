52efad6c0b7172677ad503a2b41fb93209f527aa
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-03-06 17:43:23 +0900

    chore: tsc section2/index.ts

---
+++ section2/index.js
@@ -0,0 +1,38 @@
+var hasValue = true;
+var count = 12;
+var float = 3.14;
+var negative = -1;
+var single = 'hello';
+var double = "hello";
+var back = "hello";
+// objectに型を付ける例
+var person = {
+    name: 'Jack',
+    age: 21
+};
+// TypeScriptの型推論に任せた例
+// const person = {
+//   name: 'Jack',
+//   age: 21
+// }
+console.log(person);
+// 配列に型を付ける例
+var fruits = ['Apple', 'Banana', 'Grape'];
+console.log(fruits);
+// Tuple型の例
+var book = ['business', 1500, false];
+console.log(book);
+// Enum型の例
+var CoffeeSize;
+(function (CoffeeSize) {
+    CoffeeSize["SHORT"] = "SHORT";
+    CoffeeSize["TALL"] = "TALL";
+    CoffeeSize["GRANDE"] = "GRANDE";
+    CoffeeSize["VENTI"] = "VENTI";
+})(CoffeeSize || (CoffeeSize = {}));
+var coffee = {
+    hot: true,
+    size: CoffeeSize.TALL
+};
+coffee.size = CoffeeSize.GRANDE;
+console.log(coffee);
