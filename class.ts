12b6f7b1ed926a21574a79b4231243eacf56b093
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-03-07 15:10:03 +0900

    feat: Classのプロパティにprivate修飾子を付けたサンプルコードを追加

--- class.ts
+++ class.ts
@@ -1,21 +1,20 @@
 // ES5以前にクラスを作る方法としてあったコンストラクタ関数のES6以降版のシンタックスシュガー
 class Person {
   name: string;
+  private age: number;
 
-  constructor(initName: string) {
+  constructor(initName: string, initAge: number) {
     this.name = initName;
+    this.age = initAge;
   }
-
+  increment_age() {
+    this.age += 1;
+  }
   greet(this: Person) {
-    console.log(`Hello! My name is ${this.name}!!`);
+    console.log(`Hello! My name is ${this.name}!! I am ${this.age} years old!!!`);
   }
 }
 
-const john: Person = new Person('John');
+const john: Person = new Person('John', 40);
+john.increment_age();
 john.greet();
-
-const paul = {
-  name: 'paul',
-  greet: john.greet
-}
-paul.greet();
--- dist/class.js
+++ dist/class.js
@@ -1,18 +1,18 @@
 "use strict";
 // ES5以前にクラスを作る方法としてあったコンストラクタ関数のES6以降版のシンタックスシュガー
 var Person = /** @class */ (function () {
-    function Person(initName) {
+    function Person(initName, initAge) {
         this.name = initName;
+        this.age = initAge;
     }
+    Person.prototype.increment_age = function () {
+        this.age += 1;
+    };
     Person.prototype.greet = function () {
-        console.log("Hello! My name is " + this.name + "!!");
+        console.log("Hello! My name is " + this.name + "!! I am " + this.age + " years old!!!");
     };
     return Person;
 }());
-var john = new Person('John');
+var john = new Person('John', 40);
+john.increment_age();
 john.greet();
-var paul = {
-    name: 'paul',
-    greet: john.greet
-};
-paul.greet();
