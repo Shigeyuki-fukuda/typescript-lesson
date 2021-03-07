670ce366ad637bc4935a2edee45f4262211f8994
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-03-07 16:47:47 +0900

    feat: interfaceの記法をtypeエイリアスで書いた場合のサンプルコードを実装

---
+++ dist/interface.js
@@ -0,0 +1,6 @@
+"use strict";
+var human = {
+    name: "Paul",
+    age: 78
+};
+console.log(human);
---
+++ interface.ts
@@ -0,0 +1,9 @@
+type Human = {
+  name: string,
+  age: number
+}
+const human: Human = {
+  name: "Paul",
+  age: 78
+}
+console.log(human);
