823b3bc8953f2eea80a17b2efc6fcb76dad97dfb
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-03-08 23:04:11 +0900

    feat: Type guardの例として、typeof / in / instanceof の3つの記法を使ったサンプルコードを追加

--- advanced.ts
+++ advanced.ts
@@ -22,3 +22,46 @@ type NumberBoolean = number | boolean;
 type StringNumber = string | number;
 // 2つの型の重なる部分が number なので type Mixの型推論結果は number になる
 type Mix = NumberBoolean & StringNumber;
+
+// 条件文を使って型を絞り込む Type guard の例1
+function toUpperCase(x: string | number) {
+  if (typeof x === 'string') {
+    return x.toUpperCase();
+  }
+  return '';
+}
+// Type guardの例2
+type NomadWorker = Engineer | Blogger;
+function describeProfile(nomadWorker: NomadWorker) {
+  console.log(nomadWorker);
+  // nomadWorkerというオブジェクトの中に role という keyがあるのかの判定
+  if ('role' in nomadWorker) {
+    console.log(nomadWorker.role);
+  }
+  if ('follower' in nomadWorker) {
+    console.log(nomadWorker.follower);
+  }
+}
+
+// Type guardの例3
+class Dog {
+  speak() {
+    console.log('bow-wow!!!');
+  }
+}
+class Bird {
+  speak() {
+    console.log('tweet-tweet');
+  }
+  fly() {
+    console.log('flutter');
+  }
+}
+type Pet = Dog | Bird;
+function havePet(pet: Pet) {
+  pet.speak();
+  if (pet instanceof Bird) {
+    pet.fly();
+  }
+}
+havePet(new Bird());
--- dist/advanced.js
+++ dist/advanced.js
@@ -5,3 +5,47 @@ var quil = {
     follower: 1000
 };
 console.log(quil);
+// 条件文を使って型を絞り込む Type guard の例1
+function toUpperCase(x) {
+    if (typeof x === 'string') {
+        return x.toUpperCase();
+    }
+    return '';
+}
+function describeProfile(nomadWorker) {
+    console.log(nomadWorker);
+    // nomadWorkerというオブジェクトの中に role という keyがあるのかの判定
+    if ('role' in nomadWorker) {
+        console.log(nomadWorker.role);
+    }
+    if ('follower' in nomadWorker) {
+        console.log(nomadWorker.follower);
+    }
+}
+// Type guardの例3
+var Dog = /** @class */ (function () {
+    function Dog() {
+    }
+    Dog.prototype.speak = function () {
+        console.log('bow-wow!!!');
+    };
+    return Dog;
+}());
+var Bird = /** @class */ (function () {
+    function Bird() {
+    }
+    Bird.prototype.speak = function () {
+        console.log('tweet-tweet');
+    };
+    Bird.prototype.fly = function () {
+        console.log('flutter');
+    };
+    return Bird;
+}());
+function havePet(pet) {
+    pet.speak();
+    if (pet instanceof Bird) {
+        pet.fly();
+    }
+}
+havePet(new Bird());
