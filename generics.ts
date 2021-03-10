0fdd74d30982fde2cf75d1386fdbac9429fd1c1e
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-03-10 19:24:43 +0900

    feat: ジェネリクスにextendsを付けて型パラメータに制約を付けるようにした

--- generics.ts
+++ generics.ts
@@ -1,6 +1,6 @@
 // ジェネリクスの例: T は type として一般的に用いられる
 // これを使うことで汎用的な関数を作ることが可能
-function copy<T>(value: T): T {
+function copy<T extends { name: string }>(value: T): T {
   return value;
 }
 console.log(copy({ name: 'John' }));
