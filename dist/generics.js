1c4ce40fef154b70cffa9e5096829cf17975e29f
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-03-10 23:20:37 +0900

    feat: keyof 演算子を使ってオブジェクトのkeyのUnion型を作成するようにした

--- dist/generics.js
+++ dist/generics.js
@@ -1,7 +1,8 @@
 "use strict";
 // ジェネリクスの例: T は type として一般的に用いられる
 // これを使うことで汎用的な関数を作ることが可能
-function copy(value) {
+function copy(value, key) {
+    console.log(value[key]);
     return value;
 }
-console.log(copy({ name: 'John' }));
+console.log(copy({ name: 'John', age: 38 }, 'age'));
--- generics.ts
+++ generics.ts
@@ -1,6 +1,7 @@
 // ジェネリクスの例: T は type として一般的に用いられる
 // これを使うことで汎用的な関数を作ることが可能
-function copy<T extends { name: string }>(value: T): T {
+function copy<T extends { name: string }, U extends keyof T>(value: T, key: U): T {
+  console.log(value[key]);
   return value;
 }
-console.log(copy({ name: 'John' }));
+console.log(copy({ name: 'John', age: 38 }, 'age'));
